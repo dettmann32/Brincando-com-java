8377d3454f9878eb45e6b64f2ffdfc5300a11b57
Lucas <lucasdettmann8@gmail.com>
2024-10-27 16:44:04 -0300

    praticando lambdas

--- java3/lambda.java
+++ java3/lambda.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import interfaces.lambdaFunc;
 
 class lambda{
@@ -9,6 +13,28 @@ class lambda{
         System.out.println(func.test("aba"));
 
         System.out.println(func.getS());
+
+        List<String> lista = new ArrayList<>();
+
+        lista.add("eu");
+        lista.add("mais alguem");
+        lista.add("e vc");
+
+        
+        //System.out.println(lista.removeIf(data -> lista.get(0) == "eu"));
+
+        System.out.println(lista);
+    
+        Iterator<String> listaI = lista.iterator();
+
+        if(listaI.hasNext()){
+            String value = listaI.next();
+            lista.remove(value);
+            
+        }
+
+        System.out.println();
+
 
     }
 }
